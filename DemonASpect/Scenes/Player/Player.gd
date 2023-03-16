0b757ade8a01bd1ed0ea88c183913f392f199808
radagon828 <leddy@madisoncollege.edu>
2023-03-15 22:58:45 -0500

    attempted to get attack animation working

--- DemonASpect/Scenes/Player/Player.gd
+++ DemonASpect/Scenes/Player/Player.gd
@@ -16,6 +16,8 @@ enum {
 var state = MOVE
 var roll_vector = Vector2.RIGHT
 var jumpTerminationMultiplier = 3
+
+#BOOLS
 var hasDoubleJump = false
 # velcoity is a predefined variable in godot 4
 @onready var animationPlayer = $AnimationPlayer
@@ -33,7 +35,8 @@ func _physics_process(delta: float):
 			move_state(delta)
 		ROLL:
 			roll_state(delta)
-	update_sprite()
+#		ATTACK:
+#			attack_state(delta)
 	debug()
 
 func get_input_vector():
@@ -44,9 +47,11 @@ func get_input_vector():
 
 func move_state(delta):
 	var inputVector = get_input_vector()
-
+	
+	#RUNNING
 	running(inputVector, delta)
 	velocity.x = clamp(velocity.x, -MAX_SPEED, MAX_SPEED)
+	
 	#JUMPING
 	if (inputVector.y < 0 && (is_on_floor() || hasDoubleJump)):
 		velocity.y = inputVector.y * JUMP_POWER
@@ -59,49 +64,76 @@ func move_state(delta):
 	else:
 		velocity.y += GRAVITY * delta
 	
-	if (velocity.y > 0):
-		$AnimationTree.set("parameters/in_air/transition_request", "falling")
-
 	if (is_on_floor()):
 		hasDoubleJump = true
 		
+	if (velocity.y > 0):
+		$AnimationTree.set("parameters/in_air/transition_request", "falling")
+
 	$AnimationTree.set("parameters/in_air_state/transition_request", !is_on_floor())
 	
+	#ROLL STATE SWITCH
 	if (Input.is_action_just_pressed("roll") && is_on_floor()):
 		$DashTimer.start()
 		state = ROLL
 	
+	#processing for run stop animation
+	if (Input.is_action_just_pressed("move_left") || Input.is_action_just_pressed("move_right")):
+		$RunTimer.start()
+	
+	if ($RunTimer.time_left > 0.1 && $RunTimer.time_left < 0.2):
+		$AnimationTree.set("parameters/OneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+	elif (Input.is_action_just_released("move_left") || Input.is_action_just_released("move_right")):
+		$RunTimer.stop()
+		if ($RunTimer.time_left > 0.2):
+			$AnimationTree.set("parameters/OneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_ABORT)
+	
+	#ATTACK STATE SWITCH
+	if (Input.is_action_just_pressed("attack") && is_on_floor()):
+		state = ATTACK
+		
+	
+	update_sprite()
 	move()
 
 func running(inputVector, delta):
 	if (inputVector.x != 0):
 		roll_vector.x = inputVector.x
-		$AnimationTree.set("parameters/movement/transition_request", "run")
-		if abs(velocity.x) == MAX_SPEED:
-			$AnimationTree.set("parameters/OneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
 		velocity.x += inputVector.x * ACCELERATION * delta
+		$AnimationTree.set("parameters/movement/transition_request", "run")
 	elif inputVector.x == 0:
 		$AnimationTree.set("parameters/movement/transition_request", "idle")
 		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
-		
+	
+	
 
-#find out how to slow down roll
+#NEXT--maybe cancel roll animation with runn or attack input
 func roll_state(delta):
 	$AnimationTree.set("parameters/movement/transition_request", "roll")
 	if ($DashTimer.time_left > 0.1):
 		velocity.x = roll_vector.x * ROLL_SPEED
 #	velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
-	velocity.x = lerp(0.0, velocity.x, pow(2, -6 * delta))
+	velocity.x = lerp(0.0, velocity.x, pow(2, -8 * delta))
 	
 	velocity.y += GRAVITY * delta
 	move()
 	
 func on_roll_finished():
-	print("hello i worked")
-#	velocity.x = 0
 	state = MOVE
 	$AnimationTree.set("parameters/movement/transistion_request", "idle")
-
+#
+#func attack_state(delta):
+#	$AnimationTree.set("parameters/movement/transistion_request", "attack")
+#	print("hello i worked")
+##	velocity.x = roll_vector.x * ROLL_SPEED
+#
+#	velocity.y += GRAVITY * delta
+#	move()
+#
+#func on_attack_finished():
+#	state = MOVE
+#	$AnimationTree.set("parameters/movement/transistion_request", "idle")
+	
 func move():
 	set_up_direction(Vector2.UP)
 	move_and_slide()
@@ -114,8 +146,8 @@ func update_sprite():
 func debug():
 	pass
 #	print(!$RayCast2D.is_colliding())
-	print(velocity, $DashTimer.time_left)
+#	print(velocity, $DashTimer.time_left, "         ", $RunTimer.time_left)
 #	print(is_on_floor())
-#	print(animationPlayer.current_animation_position)
+	
 
-
+	
