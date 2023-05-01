78cfbe226bacfac6a68ea3c6aa4b20e5f07d47fe
radagon828 <leddy@madisoncollege.edu>
2023-04-30 22:45:53 -0500

    got second enemy to chase and even a hurt state, however i need to figure out how the fuckin timer works again

--- DemonASpect/Scenes/Enemies/rat_man.gd
+++ DemonASpect/Scenes/Enemies/rat_man.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
-const max_speed = 125
-const gravity = 300
+const maxSpeed = 55
+const gravity = 500
+const FRICTION = 200
 
 enum {
 	IDLE,
@@ -11,10 +12,13 @@ enum {
 }
 
 var state = IDLE
+var knockback = Vector2.RIGHT
 
 @onready var playerDetectionZone = $PlayerDetectionZone
 @onready var stats = $Stats
 @onready var animationPlayer = $AnimationPlayer
+@onready var ratSprite = $Sprite2D
+@onready var timer = $Timer
 
 func _ready():
 	velocity = velocity
@@ -25,18 +29,58 @@ func _physics_process(delta):
 	match state:
 		IDLE:
 			idle_state(delta)
+			seek_player()
 		CHASE:
-			pass
+			chase_state(delta)
 		ATTACK:
 			pass
 		HURT:
-			pass
+			hurt_state(delta)
+	move()
+	update_sprite()
+	velocity.y += gravity * delta
+	print(timer.time_left)
 
 func idle_state(delta):
 	animationPlayer.play("idle")
+	velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+	
+func seek_player():
+	if playerDetectionZone.can_see_player():
+		state = CHASE
+		pass
+
+func chase_state(delta):
+	var player = playerDetectionZone.player
+	if player != null:
+		accelerate_towards_point(player.global_position, delta)
+	else:
+		state = IDLE
+
+func accelerate_towards_point(point, delta):
+	var direction = global_position.direction_to(point)
+	var distance = point.x - global_position.x
+	animationPlayer.play("chase")
+	print(distance)
+	velocity.x = velocity.move_toward(direction * maxSpeed, 300 * delta).x
+
+func hurt_state(delta):
+	timer.start()
+	animationPlayer.play("hurt")
+	velocity.x = lerp(0.0, velocity.x, pow(2, -16 * delta))
+	if timer.time_left < 0.1 : state = IDLE
 
 func on_hurtbox_entered(area: Area2D):
+	velocity = Vector2.ZERO
+	knockback = area.knockback_vector * 60
+	velocity += knockback
 	stats.health -= 1
+	state = HURT
+
+func update_sprite():
+	if (velocity.x != 0):
+		ratSprite.flip_h = true if velocity.x < 0 else false
+
 
 func move():
 	set_up_direction(Vector2.UP)
