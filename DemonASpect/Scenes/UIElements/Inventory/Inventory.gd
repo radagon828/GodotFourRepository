ccf3f7b92ed15993391e91835f8f4e3b173014b4
radagon828 <leddy@madisoncollege.edu>
2023-06-22 21:45:12 -0500

    fixed bugs

---
+++ DemonASpect/Scenes/UIElements/Inventory/Inventory.gd
@@ -0,0 +1,29 @@
+extends Resource
+class_name Inventory
+
+var drag_data = null
+
+signal items_changed(indexes)
+
+@export var items: Array[Resource] = [
+	null, null, null, null, null, null, null, null, null
+]
+
+func set_item(item_index, item):
+	var previousItem = items[item_index]
+	items[item_index] = item
+	emit_signal("items_changed", [item_index])
+	return previousItem
+	
+func swap_items(item_index, target_item_index):
+	var targetItem = items[target_item_index]
+	var item = items[item_index]
+	items[target_item_index] = item
+	items[item_index] = targetItem
+	emit_signal("items_changed", [item_index, target_item_index])
+	
+func remove_item(item_index):
+	var previousItem = items[item_index]
+	items[item_index] = null
+	emit_signal("items_changed", [item_index])
+	return previousItem
