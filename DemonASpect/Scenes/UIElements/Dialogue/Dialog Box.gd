1c13511bc9df04791d7c221cb6a19d92a0c558fe
radagon828 <leddy@madisoncollege.edu>
2023-07-06 03:24:19 -0500

    deciding on what dialog tutorial i want, probably heartbeast again

--- DemonASpect/Scenes/UIElements/Dialogue/Dialog Box.gd
+++ DemonASpect/Scenes/UIElements/Dialogue/Dialog Box.gd
@@ -2,3 +2,11 @@ extends ColorRect
 
 @export var dialogPath: String = ""
 @export var textSpeed: float = 0.05
+
+var dialog
+
+func _ready():
+	$Timer.wait_time = textSpeed
+	dialog = getDialog()
+	
+func getDialog() -> Array:
