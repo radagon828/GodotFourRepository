418b2cddd0ee1c83fdf63f2b5bbc0a8c3be2d979
radagon828 <leddy@madisoncollege.edu>
2023-07-08 23:12:46 -0500

    figured out how to create a basic dialogue box, will need to find a way to use resources in order to create a game wide system

---
+++ DemonASpect/Scenes/UIElements/Dialogue/DialogBox.gd
@@ -0,0 +1,24 @@
+extends RichTextLabel
+
+var dialog: Array[String] = ["Hey! My name is Hangman Adam Page and I'm going to train you!", "Go to Bed"]
+var page = 0
+@onready var timer = $Timer
+
+func _ready():
+	parse_bbcode(dialog[page])
+	visible_characters = 0
+	set_process_input(true)
+	
+func _input(event):
+	if Input.is_action_just_released("ui_accept"):
+		if visible_characters > get_total_character_count():
+			if page < dialog.size()-1:
+				page += 1
+				parse_bbcode(dialog[page])
+				visible_characters = 0
+		else:
+			visible_characters = get_total_character_count()
+	
+
+func _on_timer_timeout():
+	visible_characters = visible_characters + 1
