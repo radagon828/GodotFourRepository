f06c3b2170d555cecc4dfebda63639b0de67a386
radagon828 <leddy@madisoncollege.edu>
2023-09-18 21:32:15 -0500

    finished heartbeats lesson 1 godot 4 video

--- Heart Platformer/player.gd
+++ Heart Platformer/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@export var animPlayer: AnimationPlayer
+@export var sprite2d : Sprite2D
 
 const SPEED = 100.0
 const ACCELERATION = 800
@@ -13,12 +15,11 @@ func _physics_process(delta):
 	apply_gravity(delta)
 	handle_jump()
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_axis = Input.get_axis("ui_left", "ui_right")
 	handle_acceleration(input_axis, delta)
 	apply_friction(input_axis, delta)
-
+	
+	update_animations(input_axis)
 	move_and_slide()
 	
 func apply_gravity(delta):
@@ -27,10 +28,10 @@ func apply_gravity(delta):
 		
 func handle_jump():
 	if is_on_floor():
-		if Input.is_action_just_pressed("ui_accept"):
+		if Input.is_action_just_pressed("ui_up"):
 			velocity.y = JUMP_VELOCITY
 	else: 
-		if Input.is_action_just_released("ui_accept") and velocity.y < JUMP_VELOCITY / 2:
+		if Input.is_action_just_released("ui_up") and velocity.y < JUMP_VELOCITY / 2:
 			velocity.y = JUMP_VELOCITY / 2
 
 func apply_friction(input_axis, delta):
@@ -40,4 +41,15 @@ func apply_friction(input_axis, delta):
 func handle_acceleration(input_axis, delta):
 	if input_axis:
 		velocity.x = move_toward(velocity.x, SPEED * input_axis, ACCELERATION * delta)
-	
+
+func update_animations(input_axis):
+	if input_axis != 0:
+		sprite2d.flip_h = (input_axis < 0)
+		animPlayer.play("Run")
+	else:
+		animPlayer.play("Idle")
+		
+	if not is_on_floor():
+		animPlayer.play("Jump")
+		
+		
--- Heart Platformer/world.gd
+++ Heart Platformer/world.gd
@@ -5,3 +5,4 @@ extends Node2D
 
 func _ready():
 	polygon_2d.polygon = collision_polygon_2d.polygon
+	RenderingServer.set_default_clear_color(Color.BLACK)
