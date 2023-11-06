476e2e3af5b8e1278b366481b835fa037d0731ef
radagon828 <leddy@madisoncollege.edu>
2023-11-05 21:45:36 -0600

    added auto tiling to heart platformer

--- Heart Platformer/world.gd
+++ Heart Platformer/world.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
-@onready var collision_polygon_2d = $StaticBody2D/CollisionPolygon2D
-@onready var polygon_2d = $StaticBody2D/CollisionPolygon2D/Polygon2D
+#@onready var collision_polygon_2d = $StaticBody2D/CollisionPolygon2D
+#@onready var polygon_2d = $StaticBody2D/CollisionPolygon2D/Polygon2D
 @onready var level_completed = $CanvasLayer/LevelCompleted
 
 func _ready():
-	polygon_2d.polygon = collision_polygon_2d.polygon
+#	polygon_2d.polygon = collision_polygon_2d.polygon
 	RenderingServer.set_default_clear_color(Color.BLACK)
 	Events.level_completed.connect(show_level_completed)
 	
