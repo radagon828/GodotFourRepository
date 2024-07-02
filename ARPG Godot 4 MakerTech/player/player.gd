00c8f3451d88dd027e6ce3abd49e259df07ce30c
radagon828 <leddy@madisoncollege.edu>
2024-07-01 21:22:45 -0500

    catch up

---
+++ ARPG Godot 4 MakerTech/player/player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+@export var speed: int = 35
+@export var animationPLayer: AnimationPlayer
+
+
+func handleInput():
+	var moveDirection = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = moveDirection * speed
+	
+func updateAnimation():
+	if velocity.length() == 0:
+		if animationPLayer.is_playing():
+			animationPLayer.stop()
+	else:
+		var direction = "Down"
+		if velocity.x < 0: direction = "Left"
+		elif velocity.x > 0: direction = "Right"
+		elif velocity.y < 0: direction = "Up"
+	
+		animationPLayer.play("walk" + direction)
+	
+func _physics_process(delta):
+	handleInput()
+	move_and_slide()
+	updateAnimation()
