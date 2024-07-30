c6c679c72833a037c6ec273e1a5fda5b6e21aab4
radagon828 <leddy@madisoncollege.edu>
2024-07-29 21:31:10 -0500

    added 3d project

---
+++ ProjectBoost/Player.gd
@@ -0,0 +1,11 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("fucking get on with it")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
