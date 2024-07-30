7f587983e3eb526b4ea4ff8ab61665f4d689f239
radagon828 <leddy@madisoncollege.edu>
2024-07-29 21:14:01 -0500

    lesson finished do assignments

--- ARPG Godot 4 MakerTech/gui/slot_gui.gd
+++ ARPG Godot 4 MakerTech/gui/slot_gui.gd
@@ -2,6 +2,7 @@ extends Panel
 
 @onready var backgroundSprite: Sprite2D = $background
 @onready var itemSprite: Sprite2D = $CenterContainer/Panel/item
+@onready var amountLabel: Label = $CenterContainer/Panel/Label
 
 #changes inventory slot to reflect the item being there by adding the item sprite
 # and also the slot sprite
@@ -9,7 +10,10 @@ func update(slot: InventorySlot):
 	if !slot.item:
 		backgroundSprite.frame = 0
 		itemSprite.visible = false
+		amountLabel.visible = false
 	else:
 		backgroundSprite.frame = 1
 		itemSprite.visible = true
 		itemSprite.texture = slot.item.texture
+		amountLabel.visible = true
+		amountLabel.text = str(slot.amount)
