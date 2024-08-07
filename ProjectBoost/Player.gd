98808fc68a16b3731ed3277ba103b77d1117f60d
radagon828 <leddy@madisoncollege.edu>
2024-08-07 16:59:20 -0500

    did some lessons on 3d

--- ProjectBoost/Player.gd
+++ ProjectBoost/Player.gd
@@ -1,11 +1,9 @@
 extends Node3D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	print("fucking get on with it")
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_pressed("ui_accept"):
+		position.y += delta
+	if Input.is_action_pressed("ui_left"):
+		rotate_z(delta)
