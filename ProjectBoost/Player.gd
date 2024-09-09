2574b4d77c5289ce2a8088ee0f82cd694179c774
radagon828 <leddy@madisoncollege.edu>
2024-09-08 21:31:59 -0500

    can control polygon like rocket, may have decided that i won't do any maker tech videos until i need to build a proper UI again

--- ProjectBoost/Player.gd
+++ ProjectBoost/Player.gd
@@ -1,9 +1,18 @@
-extends Node3D
+extends RigidBody3D
 
+
+#axis lock is how oyu make 2d games in 3d by locking the axis you can move in
+
+#basis.y is local up wich means local to the player, which means where ever the top of the player is is up
+#global up is up from the whole world and local_up is up from the top of the object
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_accept"):
-		position.y += delta
+		apply_central_force(basis.y * delta * 1000)
+		
 	if Input.is_action_pressed("ui_left"):
-		rotate_z(delta)
+		apply_torque(Vector3(0.0, 0.0, 100.00 * delta))
+		
+	if Input.is_action_pressed("ui_right"):
+		apply_torque(Vector3(0.0, 0.0, -100.00 * delta))
