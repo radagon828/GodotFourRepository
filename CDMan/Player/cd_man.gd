36f9b11bd9feb925db9a1b5000cee6625ce244d7
radagon828 <leddy@madisoncollege.edu>
2024-10-16 21:51:17 -0500

    added running and jumping animations

--- CDMan/Player/cd_man.gd
+++ CDMan/Player/cd_man.gd
@@ -1,28 +1,59 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 150.0
+const JUMP_VELOCITY = -200.0
+var gravity = 500
 
+@export var sprites: Array[Node] 
+@export var theTorso: AnimationPlayer
+@export var theLegs: AnimationPlayer
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+
+func _ready() -> void:
+	sprites.append_array($Sprites.get_children())
 
 func _physics_process(delta):
-	# Add the gravity.
+	
+	#GRAVITY
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	
+	var direction = get_input_vector()
+	if direction.x:
+		velocity.x = direction.x * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		
+	handle_jump()
+	animate()
+	flip()
 	move_and_slide()
+	
+func get_input_vector():
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	input_vector.y = Input.get_action_strength("move_up") - Input.get_action_strength("move_down")
+	return input_vector
+	
+func handle_jump():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+func animate():
+	if is_on_floor() && velocity.x != 0:
+		theTorso.play("runningTorso")
+		theLegs.play("runningLegs")
+	elif is_on_floor():
+		theTorso.play("idleTorso")
+		theLegs.play("idleLegs")
+	elif not is_on_floor():
+		theTorso.play("jumpTorso")
+		theLegs.play("jumpLegs")
+		
+func flip():
+	if velocity.x != 0:
+		for i in sprites.size():
+			sprites[i].flip_h = !bool(sign(velocity.x) + 1)
+
