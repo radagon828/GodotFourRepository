a0fd060c982b0ae9de71847b8df81df4809ca0b2
radagon828 <leddy@madisoncollege.edu>
2024-12-26 17:03:22 -0600

    started up 3d lessons again

---
+++ ProjectBoost3DLessons/Player.gd
@@ -0,0 +1,12 @@
+extends Node3D
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("ui_accept"):
+		position.y += delta
+	
+	if Input.is_action_pressed("ui_left"):
+		rotate_z(delta)
+	if Input.is_action_pressed("ui_right"):
+		rotate_z(-delta)
