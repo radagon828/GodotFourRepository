96f426a10abe0f2e07708ace38ab7ccca03bce24
radagon828 <leddy@madisoncollege.edu>
2024-12-28 16:40:47 -0600

    added tweening to 3d boost project

--- ProjectBoost3DLessons/Player.gd
+++ ProjectBoost3DLessons/Player.gd
@@ -4,6 +4,8 @@ extends RigidBody3D
 
 @export_range(50.0, 300.0) var torque_thrust: float = 100 
 
+var isTransitioning: bool = false
+
 #basis y is whatever direction is up on the object, the direction the top of the object is
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
@@ -18,16 +20,27 @@ func _process(delta: float) -> void:
 #the landing pad has the file inside of it. attaching scenes to 
 func _on_body_entered(body: Node) -> void:
 #	print(body.name)
-	if "Goal" in body.get_groups():
-		complete_level(body.file_path)
-		print("Landed Safely!")
+	if !isTransitioning:
+		if "Goal" in body.get_groups():
+			complete_level(body.file_path)
+			print("Landed Safely!")
 	
-	if "Basic_Ground" in body.get_groups():
-		print("CRASH!!!")
-		crash_sequence()
+		if "Basic_Ground" in body.get_groups():
+			print("CRASH!!!")
+			crash_sequence()
 		
 func crash_sequence() -> void:
-	get_tree().reload_current_scene()
+	var tween = create_tween()
+	isTransitioning = true
+	set_process(false)
+	tween.tween_interval(1.0)
+	tween.tween_callback(get_tree().reload_current_scene)
+#	get_tree().reload_current_scene()
 	
 func complete_level(next_level_file: String) -> void:
-	get_tree().change_scene_to_file(next_level_file)
+	isTransitioning = true
+	set_process(false)
+	var tween = create_tween()
+	tween.tween_interval(1.0)
+	tween.tween_callback(get_tree().change_scene_to_file.bind(next_level_file))
+	
