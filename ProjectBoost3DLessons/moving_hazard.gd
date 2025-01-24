9966212a999bdf64186e0e9b2c76fd1a28761c13
radagon828 <leddy@madisoncollege.edu>
2025-01-23 19:19:45 -0600

    had an existential crisis

--- ProjectBoost3DLessons/moving_hazard.gd
+++ ProjectBoost3DLessons/moving_hazard.gd
@@ -3,6 +3,7 @@ extends AnimatableBody3D
 @export var destination: Vector3
 @export var duration: float = 1.0
 # Called when the node enters the scene tree for the first time.
+#createds tween
 func _ready() -> void:
 	var tween = create_tween()
 	tween.set_loops()
@@ -10,7 +11,7 @@ func _ready() -> void:
 	tween.tween_property(self, "global_position", global_position + destination, duration)
 	tween.tween_property(self, "global_position", global_position, duration)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Called every frame. 'delta' is the elapsed time since the previous frame. 
 func _process(delta: float) -> void:
 	pass
 	
