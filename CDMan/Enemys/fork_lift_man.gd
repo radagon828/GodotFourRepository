70b0c8c2682093b7f1419b6af44acf7e1df9fa9b
radagon828 <leddy@madisoncollege.edu>
2025-03-01 22:13:10 -0600

    seperated sprites and animation players for forklift enemy

--- CDMan/Enemys/fork_lift_man.gd
+++ CDMan/Enemys/fork_lift_man.gd
@@ -1,28 +1,55 @@
 extends CharacterBody2D
 
+enum State {ATTACK, IDLE}
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var faceVector = Vector2.LEFT
+
+
+@export var bodyAni: AnimationPlayer
+@export var forkAni: AnimationPlayer
+@export var moveTimer: Timer
+#STATE VARIABLES
+var currentState
+#using this boolean allows functions in states to be called for one frame
+var isStateNew = true
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _ready() -> void:
+	currentState = State.IDLE
+	
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	match currentState:
+		State.ATTACK:
+			process_attack(delta)
+		State.IDLE:
+			process_idle(delta)
+	isStateNew = false
+	velocity.y += gravity
 	move_and_slide()
+	
+func change_state(newstate):
+	currentState = newstate
+	isStateNew = true
+	
+func process_idle(delta):
+	bodyAni.play("Idle")
+	forkAni.play("ForkLiftLowered")
+#	if 
+#	velocity.x
+	pass
+	
+func process_attack(delta):
+	pass
+
+	
+func animate_fork():
+	pass
+func animate_body():
+	pass
+	
