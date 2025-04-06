4e153fd0c27068a2762c4f8159c9215dc8ca8af7
radagon828 <leddy@madisoncollege.edu>
2025-04-06 18:50:43 -0500

    voxel terrain lessons

---
+++ voxelterrainmakertech/world.gd
@@ -0,0 +1,21 @@
+extends Node3D
+
+@export var world_size: Vector3 = Vector3(16, 16, 16)
+@export_range(-1, 1) var cut_off: float = 0.5
+@onready var default_cube: CSGBox3D = $DefaultCube
+
+
+func _ready() -> void:
+	var random_generator = RandomNumberGenerator.new()
+	#var random_generator = FastNoiseLite.new()
+	
+	for x in range(world_size.x):
+		for z in range(world_size.z):
+			for y in range(world_size.y):
+				var random = random_generator.randf()
+				#var random = random_generator.get_noise_3d(x, y, z)
+				if random > cut_off:
+					var new_cube = default_cube.duplicate()
+					new_cube.position = Vector3(x, y, z)
+					add_child(new_cube)
+	remove_child(default_cube)
