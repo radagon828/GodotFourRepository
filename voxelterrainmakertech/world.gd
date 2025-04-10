6df04c736bfcb447c2b41a9fa1604ad3c1580631
radagon828 <leddy@madisoncollege.edu>
2025-04-09 21:31:24 -0500

    added custom moniters, rendering is expensive on the cpu

--- voxelterrainmakertech/world.gd
+++ voxelterrainmakertech/world.gd
@@ -4,11 +4,15 @@ extends Node3D
 @export_range(-1, 1) var cut_off: float = 0.5
 @onready var default_cube: CSGBox3D = $DefaultCube
 
+var cubes: int = 0
 
 func _ready() -> void:
+	Performance.add_custom_monitor("game/cubes", func(): return cubes)
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	#var random_generator = RandomNumberGenerator.new()
 	var random_generator = FastNoiseLite.new()
+	
+	var start_time = Time.get_ticks_usec()
 	
 	for x in range(world_size.x):
 		for z in range(world_size.z):
@@ -19,7 +23,12 @@ func _ready() -> void:
 					var new_cube = default_cube.duplicate()
 					new_cube.position = Vector3(x, y, z)
 					add_child(new_cube)
+					cubes +=1
 	remove_child(default_cube)
+	
+	var end_time = Time.get_ticks_usec()
+	var gen_time = (end_time - start_time) / 1000000.0
+	print_debug("Blocks in world: %s\n Gen Time: %s" % [cubes, gen_time])
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
